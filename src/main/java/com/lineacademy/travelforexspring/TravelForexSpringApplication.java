d5032b89a0d172b4fc1592fbc72654df4da7f0cc
yeonhan <yeonhanlab@gmail.com>
2026-08-13 17:26:29 +0900

    초기화

---
+++ src/main/java/com/lineacademy/travelforexspring/TravelForexSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lineacademy.travelforexspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelForexSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelForexSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lineacademy/travelforexspring/TravelForexSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineacademy.travelforexspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelForexSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
