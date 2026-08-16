09336d5e02c672cdaf6b5645c63764d80792d5ab
dvpsky <haneulgim95@gmail.com>
2026-08-16 21:08:52 +0900

    TripRespository 주석 추가

--- src/main/java/com/lineacademy/travelforexspring/repository/TripRepository.java
+++ src/main/java/com/lineacademy/travelforexspring/repository/TripRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
+    // Spring Data JPA는 파라미터로 Pageable이 전달되면 페이징 쿼리로 인식합니다.
+    // 데이터 조회(LIMIT/OFFSET)와 전체 개수 조회(COUNT)를 함께 수행하여 페이징 메타데이터가 포함된 Page<Trip> 객체로 반환합니다.
     Page<Trip> findAllByUserIdOrderByIdDesc(Long userId, Pageable pageable);
 
     Optional<Trip> findByIdAndUserId(Long id, Long userId);
