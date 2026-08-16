cae35b3b30671d74f1076090c04bbf50a4d0e219
dvpsky <haneulgim95@gmail.com>
2026-08-16 20:45:20 +0900

    오류 수정

--- src/main/java/com/lineacademy/travelforexspring/repository/TripRepository.java
+++ src/main/java/com/lineacademy/travelforexspring/repository/TripRepository.java
@@ -4,7 +4,7 @@ import com.lineacademy.travelforexspring.domain.trip.Trip;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
