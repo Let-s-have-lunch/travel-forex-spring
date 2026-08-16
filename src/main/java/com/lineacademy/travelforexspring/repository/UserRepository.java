833071bde723ac5defbff11e86171caecfd2bc80
dvpsky <haneulgim95@gmail.com>
2026-08-16 20:32:36 +0900

    UserRepository 주석 추가

--- src/main/java/com/lineacademy/travelforexspring/repository/UserRepository.java
+++ src/main/java/com/lineacademy/travelforexspring/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.lineacademy.travelforexspring.repository;
 
+// 유저 담당 팀원분께
+// 관계 맺는것때문에 오류가 나서 임시로 생성한 파일입니다.
+// 유저 작업 시작하면 내용 삭제하고 다시 작성해주시면 될것 같아요. !
+
 import com.lineacademy.travelforexspring.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
