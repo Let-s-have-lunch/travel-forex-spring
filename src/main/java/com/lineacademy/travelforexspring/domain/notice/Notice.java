df6ff9c2bce6f7494430827d7ccd30570e3053f1
yeonhan <yeonhanlab@gmail.com>
2026-08-18 11:27:51 +0900

    Notice Entity 작성

---
+++ src/main/java/com/lineacademy/travelforexspring/domain/notice/Notice.java
@@ -0,0 +1,34 @@
+package com.lineacademy.travelforexspring.domain.notice;
+
+import com.lineacademy.travelforexspring.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "notices")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE notices SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class Notice extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Builder
+    public Notice(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
