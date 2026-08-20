69257b846b81a8f2d20c9effb35c6497bd79cc40
DeveloperSky95 <haneulgim95@gmail.com>
2026-08-20 16:22:17 +0900

    SecurityConfig파일 cors허용

--- src/main/java/com/lineacademy/travelforexspring/config/SecurityConfig.java
+++ src/main/java/com/lineacademy/travelforexspring/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.lineacademy.travelforexspring.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,7 +21,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
