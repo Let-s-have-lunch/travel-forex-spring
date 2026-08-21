ff2ccada5ae2aa459d68e31297a3b707ec63fbc6
DeveloperSky95 <haneulgim95@gmail.com>
2026-08-21 11:55:56 +0900

    feat: getTripList에 status(진행중/지난여행) 필터링 추가

--- src/main/java/com/lineacademy/travelforexspring/repository/TripRepository.java
+++ src/main/java/com/lineacademy/travelforexspring/repository/TripRepository.java
@@ -5,13 +5,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
-    // Spring Data JPA는 파라미터로 Pageable이 전달되면 페이징 쿼리로 인식합니다.
-    // 데이터 조회(LIMIT/OFFSET)와 전체 개수 조회(COUNT)를 함께 수행하여 페이징 메타데이터가 포함된 Page<Trip> 객체로 반환합니다.
-    Page<Trip> findAllByUserIdOrderByIdDesc(Long userId, Pageable pageable);
+    // 1. 진행 중 (종료일이 오늘보다 크거나 같은 경우)
+    Page<Trip> findAllByUserIdAndEndDateGreaterThanEqualOrderByIdDesc(Long userId, LocalDate today, Pageable pageable);
+
+    // 2. 지난 여행 (종료일이 오늘보다 작은 경우)
+    Page<Trip> findAllByUserIdAndEndDateLessThanOrderByIdDesc(Long userId, LocalDate today, Pageable pageable);
 
     Optional<Trip> findByIdAndUserId(Long id, Long userId);
 }
